1e96464a8d678d79f9196bda7a92c554993cd536
kai9987kai <37345424+kai9987kai@users.noreply.github.com>
2018-12-16 16:50:24 +0000

    Create AutoClicker.py

---
+++ AutoClicker.py
@@ -0,0 +1,14 @@
+from PIL import ImageGrab, ImageOps
+import pyautogui
+import time
+from numpy import *
+class Coordinates():
+#Coordinates can be changed to wherever you want to AutoClick
+    replayBtn = (100,350)
+
+def restartGame():
+    pyautogui.click(Coordinates.replayBtn)
+
+while True:
+    restartGame()
+    time.sleep(0)
