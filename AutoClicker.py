ba16cdfb0bc140f645f55e3d246ec4c2c147eabf
Kai Piper <37345424+kai9987kai@users.noreply.github.com>
2018-12-20 20:07:47 +0000

    now works

--- AutoClicker.py
+++ AutoClicker.py
@@ -1,49 +1,52 @@
-from PIL import ImageGrab, ImageOps
+#from PIL import ImageGrab, ImageOps
 import pyautogui
 import time
 import tkinter as tk
 from numpy import *
 
 
+class Coordinates():
+#Coordinates can be changed to wherever you want to AutoClick
+    replayBtn = (100,350)
 
-#close button (woah)
+
+# close button (woah)
 class YourGUI(tk.Tk):
     def __init__(self):
         # inherit tkinter's window methods
         tk.Tk.__init__(self)
 
-        tk.Label(self, text="ENTER X:")\
-            .grid(row=0, column=0)
+        tk.Label(self, text="ENTER X:").grid(row=0, column=0)
         self.inputX = tk.Entry(self)
         self.inputX.grid(row=0, column=1)
-        
-        tk.Label(self, text="ENTER Y:")\
-            .grid(row=0, column=0)
+
+        tk.Label(self, text="ENTER Y:").grid(row=0, column=0)
         self.inputY = tk.Entry(self)
         self.inputY.grid(row=0, column=2)
 
-        tk.Button(self, text="convert", command=self.do_conversion)\
-            .grid(row=3, column=0, columnspan=2)
-        
-        tk.Button(self, text="exit!", command=self.EXITME)\
-            .grid(row=4, column=0, columnspan=2)
+        tk.Button(self, text="convert", command=self.do_conversion).grid(row=3, column=0, columnspan=2)
+
+        tk.Button(self, text="exit!", command=self.EXITME).grid(row=4, column=0, columnspan=2)
 
     def EXITME(self):
-        strtoint("crashME!")#crashed prog so it closes
+        exit(0)  # crashed prog so it closes
+        # strtoint("crashmE!")
 
     def do_conversion(self):
-        y = self.inputY.get()   
+        y = self.inputY.get()
         x = self.inputX.get()
-        
-        running=True
+
+        running = True
         try:
-            x=strtoint(x)
-            y=strtoint(y)
+            x = int(x)
+            y = int(y)
         except:
             print("Invalid point")
-            strtoint("crashmE!")
+            exit(0)
+            # strtoint("crashmE!")
         while running:
-            pyautogui.click(replayBtn = (x,y))
+            pyautogui.click(x, y)
+
 
 your_gui = YourGUI()
 your_gui.mainloop()
