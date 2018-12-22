f992ce9e037bf0a9d83978e206776b34a149712f
Kai Piper <37345424+kai9987kai@users.noreply.github.com>
2018-12-22 21:56:18 +0000

    Update AutoClicker.py

--- AutoClicker.py
+++ AutoClicker.py
@@ -3,7 +3,7 @@ import time
 import tkinter as tk
 import webbrowser
 from tkinter import *
-
+from tkinter import messagebox
 
 class Coordinates():
     replayBtn = (100,350)
@@ -12,11 +12,11 @@ class YourGUI(tk.Tk):
         # inherit tkinter's window methods
         tk.Tk.__init__(self)
 #Enter X field and label ⬇
-        tk.Label(self, text="ENTER X:").grid(row=0, column=3)
+        tk.Label(self, text="ENTER X:", background="#ebdbff").grid(row=0, column=3)
         self.inputX = tk.Entry(self)
         self.inputX.grid(row=0, column=1)
 #Enter Y field and label ⬇
-        tk.Label(self, text="ENTER Y:").grid(row=0, column=0)
+        tk.Label(self, text="ENTER Y:", background="#ebdbff").grid(row=0, column=0)
         self.inputY = tk.Entry(self)
         self.inputY.grid(row=0, column=4)
         # Start Button ⬇
@@ -25,7 +25,26 @@ class YourGUI(tk.Tk):
         tk.Button(self, text="exit!", command=self.EXITME).grid(row=4, column=0, columnspan=2)
         def callback():
             webbrowser.open_new(r"https://kai9987kai.github.io/AutoClicker.html")
+        def callback2():
+            webbrowser.open_new(r"https://github.com/kai9987kai/AutoClicker")
+        def callback3():
+            webbrowser.open_new(r"https://autoclicker.webstarts.com/index.html?r=20181122215206")
         tk.Button(self, text="ABOUT", command=callback).grid(row=4, column=2, columnspan=2)
+        def clicked():
+            messagebox.showinfo('CONTACT', 'Email: kai9987kai@gmail.com')
+        menu = Menu(self)
+        new_item = Menu(menu)
+        new_item.add_command(label='ABOUT', command=callback)
+        new_item.add_command(label='GITHUB PAGE', command=callback2)
+        new_item.add_command(label='CONTACT', command=clicked)
+        new_item.add_separator()
+        new_item.add_command(label='START', command=self.do_conversion)
+        new_item.add_command(label='EXIT', command =self.EXITME)
+        menu.add_cascade(label='Menu', menu=new_item)
+        new_item2 = Menu(menu)
+        new_item2.add_command(label='Tutorial', command=callback3)
+        menu.add_cascade(label='Help', menu=new_item2)
+        self.config(menu=menu)
 
     def EXITME(self):
         exit(0)  # crashed prog so it closes
@@ -45,11 +64,12 @@ class YourGUI(tk.Tk):
             # strtoint("crashmE!")
         while running:
             pyautogui.click(x, y)
-            
+
 if __name__ == '__main__':
     your_gui = YourGUI()
     your_gui.title('AutoClicker') # Set title
     your_gui.iconbitmap('favicon.ico') # Set icon
     your_gui.resizable(False, False)
+    your_gui.configure(background="#ebdbff")
     your_gui.mainloop()
 time.sleep(0)
