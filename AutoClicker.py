9e1d8ec3c0ba960804de190e18477f54eacfe3be
Kai Piper <kai9987kai@gmail.com>
2018-12-24 22:02:59 +0000

    Update AutoClicker.py

--- AutoClicker.py
+++ AutoClicker.py
@@ -6,17 +6,20 @@ from tkinter import *
 from tkinter import messagebox
 import keyboard
 
+
 class Coordinates():
-    replayBtn = (100,350)
+    replayBtn = (100, 350)
+
+
 class YourGUI(tk.Tk):
     def __init__(self):
         # inherit tkinter's window methods
         tk.Tk.__init__(self)
-#Enter X field and label ⬇
+        # Enter X field and label ⬇
         tk.Label(self, text="ENTER Y:", background="#ebdbff").grid(row=0, column=2)
         self.inputX = tk.Entry(self)
         self.inputX.grid(row=0, column=1)
-#Enter Y field and label ⬇
+        # Enter Y field and label ⬇
         tk.Label(self, text="ENTER X:", background="#ebdbff").grid(row=0, column=0)
         self.inputY = tk.Entry(self)
         self.inputY.grid(row=0, column=3)
@@ -24,16 +27,22 @@ class YourGUI(tk.Tk):
         tk.Button(self, text="start", fg='green', command=self.do_conversion).grid(row=3, column=0, columnspan=2)
         # close button ⬇
         tk.Button(self, text="exit!", fg='red', command=self.EXITME).grid(row=4, column=0, columnspan=2)
+
         def callback():
             webbrowser.open_new(r"https://kai9987kai.github.io/AutoClicker.html")
+
         def callback2():
             webbrowser.open_new(r"https://github.com/kai9987kai/AutoClicker")
+
         def callback3():
             webbrowser.open_new(r"https://autoclicker.webstarts.com/index.html?r=20181122215206")
+
         tk.Button(self, text="ABOUT", command=callback).grid(row=4, column=2, columnspan=2)
+
         def clicked():
             messagebox.showinfo('CONTACT', 'Email: kai9987kai@gmail.com')
-        #Menu Bar!! ⬇
+
+        # Menu Bar!! ⬇
         menu = Menu(self)
         new_item = Menu(menu)
         new_item.add_command(label='ABOUT', command=callback)
@@ -41,7 +50,7 @@ class YourGUI(tk.Tk):
         new_item.add_command(label='CONTACT', command=clicked)
         new_item.add_separator()
         new_item.add_command(label='START', command=self.do_conversion)
-        new_item.add_command(label='EXIT', command =self.EXITME)
+        new_item.add_command(label='EXIT', command=self.EXITME)
         menu.add_cascade(label='Menu', menu=new_item)
         new_item2 = Menu(menu)
         new_item2.add_command(label='Tutorial', command=callback3)
@@ -62,18 +71,19 @@ class YourGUI(tk.Tk):
             x = int(x)
             y = int(y)
         except:
-            print("Invalid point")
+            messagebox.showerror('Invalid point', 'Invalid point')
             exit(0)
             # strtoint("crashmE!")
         while running:
             pyautogui.click(x, y)
             if keyboard.is_pressed('s'):
                 break
-              
+
+
 if __name__ == '__main__':
     your_gui = YourGUI()
-    your_gui.title('AutoClicker') # Set title
-    your_gui.iconbitmap('favicon.ico') # Set icon
+    your_gui.title('AutoClicker')  # Set title
+    your_gui.iconbitmap('favicon.ico')  # Set icon
     your_gui.resizable(False, False)
     your_gui.configure(background="#ebdbff")
     your_gui.mainloop()
