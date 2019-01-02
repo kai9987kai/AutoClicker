c57507a9ae582a430cd94139a9242781ba0de13f
Kai Piper <kai9987kai@gmail.com>
2019-01-02 22:17:57 +0000

    Update AutoClicker.py

--- AutoClicker.py
+++ AutoClicker.py
@@ -1,3 +1,4 @@
+from win10toast import ToastNotifier
 import pyautogui
 import time
 import tkinter as tk
@@ -5,57 +6,10 @@ import webbrowser
 from tkinter import *
 from tkinter import messagebox
 import keyboard
-from win32api import *
-from win32gui import *
-import win32con
-import sys, os
-import struct
 
 def NOTIFICATION():
-    class WindowsBalloonTip:
-        def __init__(self, title, msg):
-            message_map = {
-                win32con.WM_DESTROY: self.OnDestroy,
-            }
-            # Register the Window class.
-            wc = WNDCLASS()
-            hinst = wc.hInstance = GetModuleHandle(None)
-            wc.lpszClassName = "PythonTaskbar"
-            wc.lpfnWndProc = message_map  # could also specify a wndproc.
-            classAtom = RegisterClass(wc)
-            # Create the Window.
-            style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-            self.hwnd = CreateWindow(classAtom, "Taskbar", style, \
-                                     0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
-                                     0, 0, hinst, None)
-            UpdateWindow(self.hwnd)
-            iconPathName = os.path.abspath(os.path.join(sys.path[0], "favicon.ico"))
-            icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
-            try:
-                hicon = LoadImage(hinst, iconPathName, \
-                                  win32con.IMAGE_ICON, 0, 0, icon_flags)
-            except:
-                hicon = LoadIcon(0, win32con.IDI_APPLICATION)
-            flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-            nid = (self.hwnd, 0, flags, win32con.WM_USER + 20, hicon, "tooltip")
-            Shell_NotifyIcon(NIM_ADD, nid)
-            Shell_NotifyIcon(NIM_MODIFY, \
-                             (self.hwnd, 0, NIF_INFO, win32con.WM_USER + 20, \
-                              hicon, "Balloon  tooltip", msg, 200, title))
-            # self.show_balloon(title, msg)
-            time.sleep(10)
-            DestroyWindow(self.hwnd)
-
-        def OnDestroy(self, hwnd, msg, wparam, lparam):
-            nid = (self.hwnd, 0)
-            Shell_NotifyIcon(NIM_DELETE, nid)
-
-
-    def balloon_tip(title, msg):
-        w = WindowsBalloonTip(title, msg)
-
-    if __name__ == '__main__':
-        balloon_tip("AutoClicker", "V1.0")
+    toaster = ToastNotifier()
+    toaster.show_toast("AutoClicker", "V1.0")
 
 class Coordinates():
     replayBtn = (100, 350)
