7230f7aa8420cb698d470f3ae0cf77e0b3da3ff0
Kai Piper <kai9987kai@gmail.com>
2022-12-19 17:31:49 +0000

    working colour clicker

--- beta.py
+++ beta.py
@@ -1,26 +1,11 @@
 import pyautogui
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
 import threading
-
-# Import the required modules only when necessary
-try:
-    import gspread
-    from oauth2client.service_account import ServiceAccountCredentials
-    from pprint import pprint
-except ImportError:
-    pass
-
-try:
-    import win10toast
-except ImportError:
-    pass
 
 try:
     from PIL import ImageGrab
     from colormap import rgb2hex
-    from win32api import GetSystemMetrics
 except ImportError:
     pass
 
@@ -31,20 +16,16 @@ def colour_clicker():
     master.geometry("+300+300")
     master.attributes("-topmost", True)
     master.resizable(False, False)
-    try:
-        master.iconbitmap('favicon.ico')
-    except tk.TclError:
-        pass
 
     # Create the Entry widgets and the start button
-    ttk.Label(master, text="Enter color in hexadecimals:").grid(row=0, sticky=W)
-    ttk.Label(master, text="Enter start cordinate in x axis:").grid(row=1, sticky=W)
-    ttk.Label(master, text="Enter stop cordinate in x axis:").grid(row=2, sticky=W)
-    ttk.Label(master, text="Enter start cordinate in y axis:").grid(row=3, sticky=W)
-    ttk.Label(master, text="Enter end cordinate in y axis:").grid(row=4, sticky=W)
+    ttk.Label(master, text="Enter color in hexadecimals:").grid(row=0, sticky=tk.W)
+    ttk.Label(master, text="Enter start cordinate in x axis:").grid(row=1, sticky=tk.W)
+    ttk.Label(master, text="Enter stop cordinate in x axis:").grid(row=2, sticky=tk.W)
+    ttk.Label(master, text="Enter start cordinate in y axis:").grid(row=3, sticky=tk.W)
+    ttk.Label(master, text="Enter end cordinate in y axis:").grid(row=4, sticky=tk.W)
     ttk.Button(master, text="start", command=lambda: start_clicking(color_entry.get(), x_start_entry.get(),
                                                                     x_end_entry.get(), y_start_entry.get(),
-                                                                    y_end_entry.get())).grid(row=5, sticky=W)
+                                                                    y_end_entry.get())).grid(row=5, sticky=tk.W)
 
     color_entry = ttk.Entry(master)
     color_entry.grid(row=0, column=1)
@@ -64,47 +45,58 @@ def colour_clicker():
     x_start_entry.bind('<Return>', lambda event: start_clicking(color_entry.get(), x_start_entry.get(),
                                                              x_end_entry.get(), y_start_entry.get(),
                                                              y_end_entry.get()))
-    x_end_entry.bind('<Return>', lambda event: start_clicking(color_entry.get(), x_start_entry.get(),
-                                                             x_end_entry.get(), y_start_entry.get(),
-                                                             y_end_entry.get()))
-    y_start_entry.bind('<Return>', lambda event: start_clicking(color_entry.get(), x_start_entry.get(),
-                                                             x_end_entry.get(), y_start_entry.get(),
-                                                             y_end_entry.get()))
-    y_end_entry.bind('<Return>', lambda event: start_clicking(color_entry.get(), x_start_entry.get(),
-                                                             x_end_entry.get(), y_start_entry.get(),
-                                                             y_end_entry.get()))
-
-    master.mainloop()
-
-def start_clicking(color, x_start, x_end, y_start, y_end):
-    # Convert the values from the Entry widgets to integers
-    x_start = int(x_start, 10)
-    x_end = int(x_end, 10)
-    y_start = int(y_start, 10)
-    y_end = int(y_end, 10)
-
-    # Start the main script in a separate thread
-    threading.Thread(target=mainscript, args=(color, x_start, x_end, y_start, y_end)).start()
-
-def mainscript(color, x_start, x_end, y_start, y_end):
-    # Main script code goes here
-        # Main script code goes here
-    while True:
-        # Capture a screenshot of the specified area of the screen
-        screenshot = ImageGrab.grab(bbox=(x_start, y_start, x_end, y_end))
+    x_end_entry.bind('<Return>', lambda event: start_clicking(color_entry.get())), x_start_entry.get(),
+    x_end_entry.get(), y_start_entry.get
+    def start_clicking(color, x_start, x_end, y_start, y_end):
+        # Start the main script in a new thread
+        main_thread = threading.Thread(target=mainscript, args=(color, x_start, x_end, y_start, y_end))
+        main_thread.start()
         
+def mainscript(color, x_start, x_end, y_start, y_end):
+    # Define the global variables
+    global stop_clicking, pixels, stop_clicking_thread
+    # Initialize the pixels variable
+    pixels = []
+    # Convert the coordinates to integers
+    x_start = int(x_start)
+    x_end = int(x_end)
+    y_start = int(y_start)
+    y_end = int(y_end)
+    # Take a screenshot of the selected area
+    screenshot = ImageGrab.grab(bbox=(x_start, y_start, x_end, y_end))
+    # Iterate over the pixels in the screenshot
+    for pixel in pixels:
+        # Check if the pixel matches the color
+        if rgb2hex(pixel) == color:
+            # Perform the click action
+            pyautogui.click(x_start + pixels.index(pixel) % (x_end - x_start),
+                            y_start + pixels.index(pixel) // (x_end - x_start))
 
+                # Sleep for 0.01 seconds
+            pyautogui.sleep(0.01)
+                # Check if the stop_clicking flag is set
+        if stop_clicking:
+            
+            # Set the stop_clicking flag to False
+            stop_clicking = False
+            # Join the stop_clicking_thread
+            stop_clicking_thread.join()
 
-        # Convert the screenshot to a list of RGB
-        tuplespixels = screenshot.getdata()
+def stop_clicking():
+    # Define the global variables
+    global stop_clicking, pixels, stop_clicking_thread
+    # Set the stop_clicking flag to True
+    stop_clicking = True
+    # Create a new thread
+    stop_clicking_thread = threading.Thread(target=stop_clicking_func)
+    # Start the thread
+    stop_clicking_thread.start()
 
-        # Iterate over the pixels in the screenshot
-        for pixel in pixels:
-            # Check if the pixel matches the specified color
-            if rgb2hex(pixel) == color:
-                # Click the mouse at the current position
-                pyautogui.click()
-                break
+def stop_clicking_func():
+    # Sleep for 1 second
+    pyautogui.sleep(1)
+    # Set the stop_clicking flag to False
+    stop_clicking = False
 
-if __name__ == '__main__':
-    colour_clicker()
+# Run the colour_clicker function
+colour_clicker()
